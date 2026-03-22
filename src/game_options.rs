1be5f2f74f94477e0969b98b158d1286e6ef3d98
peetseater <peetseater@gmail.com>
2026-03-22 11:44:37 -0400

    Tweak window size type for easier use with sdl3

--- src/game_options.rs
+++ src/game_options.rs
@@ -1,14 +1,14 @@
 pub struct GameOptions {
-	pub window_width: usize,
-	pub window_height: usize,
+	pub window_width: u32,
+	pub window_height: u32,
 	// Eventually we can add log level when we add a real logger in
 }
 
 impl Default for GameOptions {
 	fn default() -> Self {
 		Self{
-			window_width: 640,
-			window_height: 480
+			window_width: 1280,
+			window_height: 720
 		}
 	}
 }
