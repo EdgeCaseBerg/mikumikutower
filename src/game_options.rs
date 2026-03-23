080d0c0b514370d4218936dc6d61a8a63817e47e
peetseater <peetseater@gmail.com>
2026-03-22 20:16:31 -0400

    cargo fmt

--- src/game_options.rs
+++ src/game_options.rs
@@ -1,14 +1,14 @@
 pub struct GameOptions {
-	pub window_width: u32,
-	pub window_height: u32,
-	// Eventually we can add log level when we add a real logger in
+    pub window_width: u32,
+    pub window_height: u32,
+    // Eventually we can add log level when we add a real logger in
 }
 
 impl Default for GameOptions {
-	fn default() -> Self {
-		Self{
-			window_width: 1280,
-			window_height: 720
-		}
-	}
+    fn default() -> Self {
+        Self {
+            window_width: 1280,
+            window_height: 720,
+        }
+    }
 }
