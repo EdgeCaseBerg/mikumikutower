5a001b7d566c17e3cca77b6acc6619b820ab09ed
peetseater <peetseater@gmail.com>
2026-03-28 02:32:06 -0400

    Track game name in game options
    This could make it more handily re-useable for other games later!

--- src/game_options.rs
+++ src/game_options.rs
@@ -1,4 +1,5 @@
 pub struct GameOptions {
+    pub name: String,
     pub window_width: u32,
     pub window_height: u32,
     // Eventually we can add log level when we add a real logger in
@@ -7,6 +8,7 @@ pub struct GameOptions {
 impl Default for GameOptions {
     fn default() -> Self {
         Self {
+            name: "Miku Miku Tower".to_owned(),
             window_width: 1280,
             window_height: 720,
         }
