ee259a49c5ae2e6b1468c789845a0226d3ba8f87
peetseater <peetseater@gmail.com>
2026-03-29 17:16:55 -0400

    Define Color + Rect agnostic structs

--- src/renderer.rs
+++ src/renderer.rs
@@ -4,6 +4,7 @@ use crate::game::GameContext;
 use crate::game_options::GameOptions;
 
 use std::cell::RefCell;
+use std::ops::{Add, Sub};
 use std::rc::Rc;
 
 // Colors are defined in sRGB within 0.0 - 1.0
@@ -14,6 +15,23 @@ pub struct Color {
     pub g: f32,
     pub b: f32,
     pub a: f32,
+}
+
+impl From<Color> for (u8, u8, u8, u8) {
+    fn from(color: Color) -> (u8, u8, u8, u8) {
+        let r = (color.r * 255.0) as u8;
+        let g = (color.g * 255.0) as u8;
+        let b = (color.b * 255.0) as u8;
+        let a = (color.a * 255.0) as u8;
+        (r, g, b, a)
+    }
+}
+
+impl Color {
+    pub fn to_sdl3(&self) -> sdl3::pixels::Color {
+        let (r, g, b, a) = (*self).into();
+        sdl3::pixels::Color::RGBA(r, g, b, a)
+    }
 }
 
 impl Color {
@@ -34,6 +52,36 @@ pub trait Renderer {
     fn name(&self) -> String;
     fn clear(&mut self, color: Color);
     fn present(&mut self);
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Rect<T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> = isize> {
+    pub x: T,
+    pub y: T,
+    pub width: T,
+    pub height: T,
+}
+
+impl<T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T>> Rect<T> {
+    // (x, y) are the corner from which width and height expand from (x + width = x2, similar for y)
+    // so its the bottom left corner in a positive coordinate space and the top left in a quadrant 4 space (like most images)
+    #[inline(always)]
+    pub fn new(x: T, y: T, width: T, height: T) -> Rect<T> {
+        Rect {
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+}
+
+pub enum RenderCommand {
+    DrawRect {
+        textureId: usize,
+        source: Rect,
+        destination: Rect,
+    },
 }
 
 // TODO: move to own file once done.
