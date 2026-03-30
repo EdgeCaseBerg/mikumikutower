f826474bd9870a95188c656c4bc9e20d9ed1e6ef
peetseater <peetseater@gmail.com>
2026-03-29 22:00:47 -0400

    Update GameOptions w/ assets path

--- src/game_options.rs
+++ src/game_options.rs
@@ -1,7 +1,10 @@
+use std::path::PathBuf;
+
 pub struct GameOptions {
     pub name: String,
     pub window_width: u32,
     pub window_height: u32,
+    pub assets_path: PathBuf,
     // Eventually we can add log level when we add a real logger in
 }
 
@@ -11,6 +14,7 @@ impl Default for GameOptions {
             name: "Miku Miku Tower".to_owned(),
             window_width: 1280,
             window_height: 720,
+            assets_path: PathBuf::new().join("assets"), // relative path so backends can join against it with current_dir implementations
         }
     }
 }
