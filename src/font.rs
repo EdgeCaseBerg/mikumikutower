3778c874a706930062ccfd053eebb70f27064eb9
peetseater <peetseater@gmail.com>
2026-05-09 22:51:00 -0400

    Tweak to more functional style for font creation

--- src/font.rs
+++ src/font.rs
@@ -21,14 +21,14 @@ pub fn get_rects_for_str(str: &str) -> Vec<Rect> {
     // to get the index. Then we need to convert that index into
     // This is horrifically inefficient, but for testing it should be okay:
     let cells: Vec<Rect> = FONTSHEET_LAYOUT.iter_cells().map(|(_, _, r)| r).collect();
-    let mut rects = Vec::with_capacity(str.len());
-    for c in str.chars() {
-        let ascii = c as u32;
-        if ascii > 127 {
-            rects.push(cells[0].clone());
-        } else {
-            rects.push(cells[ascii as usize - 32].clone());
-        }
-    }
-    rects
+    str.chars()
+        .map(|c| {
+            let ascii = c as u32;
+            if ascii > 127 {
+                cells[0].clone()
+            } else {
+                cells[ascii as usize - 32].clone()
+            }
+        })
+        .collect()
 }
